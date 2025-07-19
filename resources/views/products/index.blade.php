226c61d7a5634bae10bc92ad2c7f9b90d2915877
bndstocco <bndst77@gmail.com>
2025-07-19 13:01:12 -0300

    tables

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -7,6 +7,25 @@
 </head>
 <body>
     <h1>Product</h1>
-    <div>Index</div>
+    <div>
+        <table border="1" cellpadding="8" cellspacing="0">
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Qty</th>
+                <th>Price</th>
+                <th>Description</th>
+            </tr>
+            @foreach($products as $product)
+            <tr>
+                <td>{{ $product->id }}</td>
+                <td>{{ $product->name }}</td>
+                <td>{{ $product->qty }}</td>
+                <td>{{ $product->price }}</td>
+                <td>{{ $product->description }}</td>
+            </tr>
+            @endforeach
+        </table>
+    </div>
 </body>
 </html>
